fc7cdeb0b40638c4a576a574f31b70cf66de2532
flowersin <eschaefer7d4@gmail.com>
2023-03-05 14:26:37 -0700

    new file:   .project 	new file:   assets/cat1.jpeg 	new file:   assets/cat2.jpeg 	modified:   fstg.py

--- fstg.py
+++ fstg.py
@@ -12,6 +12,7 @@ BACKGROUND_SIZE = (1000, 1000)
 
 # Custom events
 BACKGROUND_CAN_CHANGE_COLOR = pg.event.custom_type()
+CHANGE_CAT = pg.event.custom_type()
 
 # Functions
 def main():
@@ -31,9 +32,20 @@ def main():
     # Set caption
     pg.display.set_caption ('Flower Sin: The Game')
     
+    # Load Images
+    IMAGE_CAT1 = pg.image.load('assets/cat1.jpeg').convert()
+    IMAGE_CAT2 = pg.image.load('assets/cat2.jpeg').convert()
+
     # Fill screen with BACKGROUND_COLOR
     MainWindow.fill(BACKGROUND_COLOR)
-    
+
+    # Display cat1.jpeg
+    MainWindow.blit(IMAGE_CAT1, (0, 0))
+    displayed_cat = 1
+
+    # Start timer for cat switching
+    pg.time.set_timer(CHANGE_CAT, 3000)
+
     # Begin game loop
     while True:
         
@@ -54,7 +66,7 @@ def main():
                 if event.key == pg.K_DOWN:
                     if background_color_change_wait == False:
                         MainWindow.fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-                        pg.time.set_timer(BACKGROUND_CAN_CHANGE_COLOR, 1000);
+                        pg.time.set_timer(BACKGROUND_CAN_CHANGE_COLOR, 1000)
                         background_color_change_wait = True
                     else:
                         print('Wait!')
@@ -64,6 +76,18 @@ def main():
             # Update background_color_change_wait
             if (event.type == BACKGROUND_CAN_CHANGE_COLOR):
                 background_color_change_wait = False
+
+            # Change displayed cat on CHANGE_CAT event
+            if (event.type == CHANGE_CAT):
+                if displayed_cat == 1:
+                    MainWindow.blit(IMAGE_CAT2, (0, 0))
+                    displayed_cat = 2
+                elif displayed_cat == 2:
+                    MainWindow.blit(IMAGE_CAT1, (0, 0))
+                    displayed_cat = 1
+                
+                # Reset the timer
+                pg.time.set_timer(CHANGE_CAT, 3000)
 
         # update display using flip
         pg.display.flip()
